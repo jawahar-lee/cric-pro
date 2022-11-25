ab5f1d2de6092998d26149b7159b0c18447cb8a7
jawahar-lee <116168249+jawahar-lee@users.noreply.github.com>
2022-11-25 15:15:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cric-pro
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-2bducb)
---
+++ script.js
@@ -0,0 +1,109 @@
+const sehwag = document.getElementById("sehwag")
+const sehwagimg = document.getElementById("sehwagimg")
+sehwag.onclick = ()  => {
+   sehwagimg.style.display = 'block';
+};
+
+const sachin = document.getElementById("sachin")
+const sachinimg = document.getElementById("sachinimg")
+sachin.onclick = () => {
+  sachinimg.style.display = 'block';
+};
+
+const gambhir = document.getElementById("gambhir")
+const gambhirimg = document.getElementById("gambhirimg")
+gambhir.onclick = () => {
+  gambhirimg.style.display = "block";
+};
+
+const king = document.getElementById("king")
+const kingimg = document.getElementById("kingimg")
+king.onclick = () => {
+  kingimg.style.display = "block";
+};
+
+const thala = document.getElementById("thala")
+const thalaimg = document.getElementById("thalaimg")
+thala.onclick = () => {
+  thalaimg.style.display = "block";
+};
+
+const yuvraj = document.getElementById("yuvraj")
+const yuvrajimg = document.getElementById("yuvrajimg")
+yuvraj.onclick = () => {
+  yuvrajimg.style.display = "block";
+};
+
+const mripl = document.getElementById("mripl")
+const mriplimg = document.getElementById("mriplimg")
+mripl.onclick = () => {
+  mriplimg.style.display = "block";
+};
+
+const harbhajan = document.getElementById("harbhajan")
+const harbhajanimg = document.getElementById("harbhajanimg")
+harbhajan .onclick = () => {
+  harbhajanimg.style.display = "block";
+};
+
+const zaheer = document.getElementById("zaheer")
+const zaheerimg = document.getElementById("zaheerimg")
+zaheer.onclick = () => {
+  zaheerimg.style.display = "block";
+};
+
+const munaf = document.getElementById("munaf")
+const munafimg = document.getElementById("munafimg")
+munaf.onclick = () => {
+  munafimg.style.display = "block";
+};
+
+const sreesanth = document.getElementById("sreesanth")
+const sreesanthimg = document.getElementById("sreesanthimg")
+sreesanth.onclick = () => {
+  sreesanthimg.style.display = "block";
+};
+
+sehwagimg.onclick = () => {
+  sehwagimg.style.display = "none";
+};
+
+sachinimg.onclick = () => {
+  sachinimg.style.display = "none";
+};
+
+gambhirimg.onclick = () => {
+  gambhirimg.style.display = "none";
+};
+
+kingimg.onclick = () => {
+  kingimg.style.display = "none";
+};
+
+thalaimg.onclick = () => {
+  thalaimg.style.display = "none";
+};
+
+yuvrajimg.onclick = () => {
+  yuvrajimg.style.display = "none";
+};
+
+mriplimg.onclick = () => {
+  mriplimg.style.display = "none";
+};
+
+harbhajanimg.onclick = () => {
+  harbhajanimg.style.display = "none";
+};
+
+zaheerimg.onclick = () => {
+  zaheerimg.style.display = "none";
+};
+
+munafimg.onclick = () => {
+  munafimg.style.display = "none";
+};
+
+sreesanthimg.onclick = () => {
+  sreesanthimg.style.display = "none";
+};
